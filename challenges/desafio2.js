b64b42995b39918127f31dce26f0411f1ad12b83
JRGCast <joao.ralph1@gmail.com>
2021-05-06 20:04:55 -0300

    feitos desafios 3 e 4

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,3 +1,8 @@
+// 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+// Para isso, escreva no arquivo desafio2.js duas queries, nesta ordem:
+
+// 1. Crie uma query que adicione o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
 db.produtos.updateMany({}, { $set: { valorUnitario: NumberDecimal("0.00") } });
 
+// 2. Crie uma query que retorne o nome e valorUnitario de todos os produtos.
 db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
---
+++ challenges/desafio3.js
@@ -0,0 +1,14 @@
+// 3 - Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo.
+// Para isso, escreva no arquivo desafio3.js quatro queries, nesta ordem:
+
+// 1. Crie uma query que inclua o campo avaliacao do tipo NumberInt e com o valor 0 em todos os documentos da coleção.
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
+
+// 2. Crie uma query que incremente o valor do campo avaliacao em 5 em todos os sanduíches de carne do tipo bovino. Dica: utilize como filtro o campo tags.
+db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $inc: { avaliacao: 5 } });
+
+// 3. Crie uma query que incremente o valor do campo avaliacao em 3 em todos os sanduíches de ave.
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
+
+// 4. Crie uma query que retorne o nome e avaliacao de todos os sanduíches.
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
