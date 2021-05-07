08fbcdb5fda1d6fc7c3707ec2b9e0272fac64921
Lucas Nascimento - MACOS <lucasmvnascimento@gmail.com>
2021-05-07 18:29:06 -0300

    Reqs 16-22

---
+++ challenges/desafio17.js
@@ -0,0 +1,7 @@
+db.produtos.createIndex({ descricao: "text" });
+
+db.produtos.find({
+  $text: {
+    $search: "frango hambúrguer",
+  },
+}).count();
---
+++ challenges/desafio18.js
@@ -0,0 +1,9 @@
+db.produtos.createIndex({ descricao: "text" });
+
+db.produtos.find(
+  {
+    $text: {
+      $search: "\"feito com\"",
+    },
+  },
+).count();
