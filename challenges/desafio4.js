e6be1c93602d97ccf02073d842d738549a9af0ce
Leandro Lachini <leandrolachini@gmail.com>
2021-05-07 16:00:01 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany({ nome: "Big Mac" }, {
+  $set: {
+    ultimaModificacao: Date() } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: false, nome: true });
