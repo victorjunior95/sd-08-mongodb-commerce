1188b338770c16ea1be5a123d226288a32e566da
victor Farias <victorfarias.tv@gmail.com>
2021-05-07 14:20:15 -0300

    Adicionado todos desafios

---
+++ challenges/desafio13.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 40 } } } }, { $push: { tags: "muito sódio" } });
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
