60f2d17509d296fe00b510dac4c4ad6f40c2b250
Leonardo <lecastro16@gmail.com>
2021-05-07 18:17:14 -0300

    requisito 11v1

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   {},
-  { $push: { tags: { $each: ["combo", "tasty"] } } },
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } },
   { upsert: true },
 );
 
