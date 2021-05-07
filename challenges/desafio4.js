cd088a15850b2fc630cdefab09e8976f1041a158
Fabio Ferreira <fabio35547@gmail.com>
2021-05-07 17:18:17 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $currentDate: {
+      ultimaModificacao: true,
+      lastModified: { $type: "date" },
+    },
+  },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
