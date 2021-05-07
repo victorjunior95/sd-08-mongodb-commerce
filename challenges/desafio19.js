46646a4c3fa3aeb3d2ee78a7aa757a10024859d7
JRGCast <joao.ralph1@gmail.com>
2021-05-06 21:39:41 -0300

    feito desafio 19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,16 +1,8 @@
 // 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
 // Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
 
-// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+// 1. Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 
-// Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
-
-// 20 - Remova o campo curtidas do item Big Mac.
-// Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
-
-// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
-
-// Crie uma query que retorne o nome e curtidas de todos os documentos.
-
-// 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
-// 22 - Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5.
+// 2. Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });
