8dc578104d4266a7c0b2d92a8733d863711c009a
JRGCast <joao.ralph1@gmail.com>
2021-05-06 22:05:25 -0300

    realizadas alteraçoes: nos desafios 9 e 22 => colocado fatores para mostrar; no desafio 4, alterado type  para true

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -2,9 +2,9 @@
 // Para isso, escreva no arquivo desafio4.js duas queries, nesta ordem:
 
 // 1. Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
-db.produtos.updateMany(
-  { nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } },
+db.produtos.updateOne(
+  { nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } },
 );
 
 // 2. Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
-db.produtos.find({ ultimaModificacao: { $exists: 1 } });
+db.produtos.find({ nome: 1, ultimaModificacao: { $exists: 1 } });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -2,7 +2,7 @@
 // Para isso, escreva no arquivo desafio9.js duas queries, nesta ordem:
 
 // 1. Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt.
-db.produtos.updateOne({ nome: "Cheddar Mcmelt" }, { $pop: { ingredientes: 1 } });
+db.produtos.updateOne({ nome: "Cheddar McMelt" }, { $pop: { ingredientes: 1 } });
 
 // Crie uma query que retorne o nome e ingredientes de todos os documentos.
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
