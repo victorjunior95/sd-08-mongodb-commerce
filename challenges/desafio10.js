bd6f75a1b911774d6f029b6c2b263b36d292ae3f
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-05-07 14:32:28 -0300

    Enviando Projeto

---
+++ challenges/desafio10.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
+);
+db.produtos.updateMany({ nome: "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany(
+  { tags: { $in: ["Bovino", "pão"] } },
+  { $set: { "vendasPorDia.6": 120 } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
