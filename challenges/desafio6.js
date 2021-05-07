d9f44a2c532f53e1d641107a8a85387a83e7e3fc
Leandro Lachini <leandrolachini@gmail.com>
2021-05-07 16:33:27 -0300

    desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, {
+  $addToSet: { ingredientes: { $each: ["bacon"] } } });
+
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
