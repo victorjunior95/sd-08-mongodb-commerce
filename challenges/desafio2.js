d11997cdaeccdf598a3ecd4ba7345bca6edcfc05
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-07 19:38:27 -0300

    exercicio 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-  { valorUnitario: { $exists: true } },
-  { $set: { voluntario: NumberDecimal("0.00") } },
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
 
 db.produtos.find({}, { nome: 1, voluntario: 1, _id: 0 });
