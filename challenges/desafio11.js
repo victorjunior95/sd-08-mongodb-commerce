4968deb8bdc19c1523fa0da05cd27f372dfddf09
Leonardo <lecastro16@gmail.com>
2021-05-07 18:49:35 -0300

    requisito 11 v5

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,7 +1,3 @@
-db.produtos.updateMany(
-  {},
-  { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } },
-  { upsert: true },
-);
+db.produtos.updateMany( {}, { $push: { tags: { $each: ["combo", "tasty"], $sort:  1 } } }, { upsert: true }, );
 
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
