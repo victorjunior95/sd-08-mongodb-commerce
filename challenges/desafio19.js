760cb614ddb5e669c2fe9f5eb4cd7db738fb8ad0
JRGCast <joao.ralph1@gmail.com>
2021-05-06 21:37:35 -0300

    feitos desafios 17 e 18, colocado 'name' nos indices

---
+++ challenges/desafio19.js
@@ -0,0 +1,16 @@
+// 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+// Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+
+// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+
+// Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+
+// 20 - Remova o campo curtidas do item Big Mac.
+// Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+// Crie uma query que retorne o nome e curtidas de todos os documentos.
+
+// 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
+// 22 - Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5.
