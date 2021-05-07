d0354c3932e52c17ef9fddc089e11cf836a2e757
Leandro Lachini <leandrolachini@gmail.com>
2021-05-07 17:26:18 -0300

    desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({}, {
   $push: { tags: { $each: ["combo", "tasty"],
-  $sort: { tags: 1 } } } });
+  $sort: 1 } } });
 
 db.produtos.find({}, { _id: false, nome: true, tags: true });
