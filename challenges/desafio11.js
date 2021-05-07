2280b9ecc25eef28a72248354f24ad89bfa1b67e
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-07 19:27:21 -0300

    Challenge 11 and 12 fixed

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -4,9 +4,9 @@ db.produtos.updateMany(
       tags: {
         $each: ["combo", "tasty"],
         $sort: 1,
-      }
+      },
     },
-  }
+  },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,12 +2,12 @@ db.produtos.updateMany(
   {},
   { 
     $push: {
-      "valoresNutricionais": {
+      valoresNutricionais: {
         $each: [],
-        $sort: { percentual: -1 } 
-      } 
+        $sort: { percentual: -1 }, 
+      }, 
     },
-  }
+  },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
