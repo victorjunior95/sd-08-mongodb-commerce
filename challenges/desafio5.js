3d78433e46481390535d017e7cdb27dcc2a1aaba
Mateus Coury <mateuscoury@gmail.com>
2021-05-07 16:00:19 -0300

    ate o 5 correcao

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,4 @@
-db.produtos.updateMany({ nome: { $exists: true, $nin: ["McChicken"] } }, { $addToSet: { ingredientes: "ketchup" } })
-db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 })
+db.produtos.updateMany({ nome: 
+  { $exists: true, $nin: ["McChicken"] } }, 
+  { $addToSet: { ingredientes: "ketchup" } });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
