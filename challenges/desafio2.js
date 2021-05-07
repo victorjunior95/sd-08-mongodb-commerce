2f0527808bcbf522291ff38cb391e5c4ec748563
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-05-07 15:07:34 -0400

    eslint req. 1 e 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,3 +1,5 @@
-db.produtos.updateMany({valorUnitario: {$exists: 0}}, {$set: {valorUnitario: NumberDecimal("0.00")}});
+db.produtos.updateMany(
+  { valorUnitario: { $exists: 0 } }, { $set: { valorUnitario: NumberDecimal("0.00") } },
+);
 
-db.produtos.find({}, {_id: 0, nome: 1, valorUnitario: 1});
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
