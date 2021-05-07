0cf3547380c10ef3ed8f27178b710f828a2ffd46
albertassi88 <albertassi88@gmail.com>
2021-05-07 19:02:30 -0300

    Alteracoes no desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,3 @@
-db.produtos.createIndex({ descricao: "text" });
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
 
 db.produtos.count({ $text: { $search: "frango hamburguer" } });
