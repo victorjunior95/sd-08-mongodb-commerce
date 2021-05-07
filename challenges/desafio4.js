3957d6894f03deb673116308329a762d26891be3
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-05-07 15:01:17 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,18 @@
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $currentDate: {
+      ultimaModificacao: true,
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: {
+      $exists: 1,
+    },
+  },
+);
