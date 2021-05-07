843b9221237e421df1c94afc88fd9bad91fc321b
Rosiele David <rosieleserpa@hotmail.com>
2021-05-07 17:27:08 -0300

    Desafio 10 concluído

---
+++ challenges/desafio10.js
@@ -0,0 +1,34 @@
+db.produtos.updateMany(
+  {}, 
+  {
+    $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+);
+
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $set: { 
+      "vendasPorDia.3": 60,
+    }, 
+  },
+);
+
+db.produtos.updateMany(
+  {
+    tags: { $in: ["bovino", "pao"] },
+  },
+  {
+    $inc: { "vendasPorDia.6": 120 },
+  },
+);
+
+db.produtos.find({}, {
+  nome: 1,
+  vendasPorDia: 1,
+  _id: 0,  
+});
