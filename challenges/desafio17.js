73a312faa6475f43820b77c00910f3256630ea92
JRGCast <joao.ralph1@gmail.com>
2021-05-06 21:15:01 -0300

    desafios 15 e 16 feitos

---
+++ challenges/desafio17.js
@@ -0,0 +1,30 @@
+// 17 - Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
+// Para isso, escreva no arquivo desafio17.js duas queries, nesta ordem:
+
+// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+
+// Crie uma query que retorne a quantidade de documentos que contêm as palavras frango e hamburguer utilizando o operador $text.
+
+// 18 - Conte quantos documentos contêm a expressão feito com utilizando o operador $text.
+// Para isso, escreva no arquivo desafio18.js duas queries, nesta ordem:
+
+// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+
+// Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
+
+// 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+// Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+
+// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+
+// Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+
+// 20 - Remova o campo curtidas do item Big Mac.
+// Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+// Crie uma query que retorne o nome e curtidas de todos os documentos.
+
+// 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
+// 22 - Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5.
