0ebc4b01af2bbd4916defc1d51bdde1b647698cf
Douglas Ferreira <dodofp.df@gmail.com>
2021-05-08 17:06:44 -0300

    Requisito 12 e 13

---
+++ challenges/desafio11.js
@@ -0,0 +1,5 @@
+// db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } }, { upsert: true });
+
+// db.protudos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } }, { upsert: true });
+
+// db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
