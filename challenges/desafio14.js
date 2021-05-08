a3a819317402097b616db07087ad6bf3cd04f5ac
MEianelli <filipelouzano@gmail.com>
2021-05-08 16:52:43 -0300

    Requisito 14 corrigido

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,5 @@
-db.produtos.updateMany({ $and: [{ "valoresNutricionais.1.percentual": { $gt: 20 } }, 
-{ "valoresNutricionais.2.percentual": { $lt: 40 } }] }, 
-{ $push: { tags: "contém sódio" } });
+db.produtos.updateMany({ valoresNutricionais: 
+  { $elemMatch: 
+    { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } }, 
+    { $push: { tags: "contém sódio" } });
 db.produtos.find({ }, { _id: 0, nome: 1, tags: 1 });
