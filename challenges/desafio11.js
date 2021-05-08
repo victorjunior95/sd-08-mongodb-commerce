6bf5f3febfdb9f4da7b77b99c7eecf529b50eeb8
Douglas Ferreira <dodofp.df@gmail.com>
2021-05-08 18:33:25 -0300

    Requisito 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,5 +1,4 @@
-// db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } }, { upsert: true });
+db.produtos.updateMany({},
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } }, { upsert: true });
 
-// db.protudos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } }, { upsert: true });
-
-// db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
