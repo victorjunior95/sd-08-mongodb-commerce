8fd291652492652969abc0a7a6c2be3231f30997
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-08 01:45:37 -0300

    req10 & 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,7 +3,7 @@ db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 
 db.produtos.updateOne({ nome: "Big Mac" },
 { $inc: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.7": 120 } });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({},
-  { _id: 0, nome: 1, vendasPorDia: 1 });
+  { _id: 0, nome: 1, vendasPorDia: 1 }); 
