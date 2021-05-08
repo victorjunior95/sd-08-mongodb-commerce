59b3bca7fbc8a05234a4371a936d5cb0233cbd68
VitorLimaRios <vitorlimarios999@gmail.com>
2021-05-08 17:46:56 -0300

    corrigindo req 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   { valoresNutricionais: { $elemMatch: 
-    { tipo: "sódio", percentual: { $elemMatch: { $gt: 20, $lt: 40 } } } } },
-  { $push: { tags: "muito sódio" } },
+    { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
+  { $push: { tags: "contém sódio" } },
 );
 
 db.produtos.find({}, { tags: 1, nome: 1, _id: 0 });
