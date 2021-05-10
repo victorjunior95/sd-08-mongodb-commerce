0c84d803a02d1926db6859988e2eb9baf3db7bfb
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-10 00:02:30 -0300

    Challenge 14 done

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,13 @@
 db.produtos.updateMany(
-  { "ValoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } },
+  { 
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gt: 20, $lt: 40 }, 
+      }, 
+    }, 
+  },
   { $push: { tags: "contém sódio" } },
 );
 
-db.produtos.find({}, { _id: 0, valoresNutricionais: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
