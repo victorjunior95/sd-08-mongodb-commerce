2aeb81312cac22b20123762633c08244af1aeaa0
Uilton Rocha <itoclick@gmail.com>
2021-05-10 04:35:56 -0300

    desafios 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({ nome: { $ne: "McChicken" } },
- { $set: { "ingredientes.$[]": "catchup" } });
+db.produtos.updateMany({ nome: { $ne: "McChicken" }, "ingredientes.$[]": { $ne: "ketchup" } },
+ { $push: { ingredientes: "catchup" } });
 
- db.produtos.find({}, { ingredientes: 1, _id: 0 }).pretty();
+ db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
