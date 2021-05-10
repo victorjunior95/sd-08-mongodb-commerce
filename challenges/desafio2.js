9292c44787a77863df89df0129007558ebdad0ae
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-05-10 13:33:44 -0300

    desafio2 - tentativa 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,3 +1,5 @@
-db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } }, { $set: { valorUnitario: NumberDecimal("0.00") } },
+  );
 
-db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
