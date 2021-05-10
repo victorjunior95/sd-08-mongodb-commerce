1a2cdcec0863dc96b24657214db0b749b627af66
Joao <joaocarlitoneto@gmail.com>
2021-05-09 22:40:02 -0300

    Req 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,1 +1,4 @@
 // 18 - Conte quantos documentos contêm a expressão feito com utilizando o operador $text.
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+
+db.produtos.find({ $text: { $search: "\"feito com\"" } }).count();
