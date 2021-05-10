0235e1d53a90ac05e48d3d6ae87ff842100cac30
Joao <joaocarlitoneto@gmail.com>
2021-05-09 21:15:18 -0300

    Req 08

---
+++ challenges/desafio8.js
@@ -0,0 +1,4 @@
+// 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
