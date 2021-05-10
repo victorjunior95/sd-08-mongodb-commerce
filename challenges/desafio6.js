ef7314eb09c22c21490ad2f97bed2868e94a3d5f
Joao <joaocarlitoneto@gmail.com>
2021-05-09 21:03:53 -0300

    Req 06

---
+++ challenges/desafio6.js
@@ -0,0 +1,6 @@
+// 6 - Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
+db.produtos.updateMany({
+   nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, { $push: { ingredientes: "bacon" }, 
+});
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
