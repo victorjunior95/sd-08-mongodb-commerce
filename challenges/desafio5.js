c996397b1bf810b33dccd84919ca9ba0e33ab949
Uilton Rocha <itoclick@gmail.com>
2021-05-10 12:42:34 -0300

    ajustando req 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({ nome: { $ne: "McChicken" }, "ingredientes.$[]": { $ne: "ketchup" } },
- { $push: { ingredientes: "catchup" } });
+db.produtos.updateMany({ ingredientes: { $ne: "ketchup" }, nome: { $ne: "McChicken" } },
+ { $push: { ingredientes: "ketchup" } });
 
- db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
