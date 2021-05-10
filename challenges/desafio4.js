6328f621a0f2b49826eaeff536c028af9b0e2c1c
amandaos <amanda.775.souza@gmail.com>
2021-05-10 19:51:00 -0300

    fix: corrige erros nos requisitios 3 e 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
-db.produtos.update({ nome: "Big Mac" }, { $set: { ultimamodificacao: Date() } });
-db.produtos.find({ ultimamodificacao: { $exists: 1 } }, { nome: 1, id_: 0 });
+db.produtos.update({ nome: "Big Mac" }, { $set: { ultimaModificacao: Date() } });
+db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { nome: 1, id_: 0 });
