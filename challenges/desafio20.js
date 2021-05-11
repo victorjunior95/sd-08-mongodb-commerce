2cf69e5f0fa7e6e27cf5e5c3ac56421a5ece5153
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-05-11 12:33:38 -0300

    req 19 ao 22

---
+++ challenges/desafio20.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $unset: {
+      curtidas: "",
+    } },
+  );
+  
+  db.produtos.find({}, { _id: false, nome: true, curtidas: true });
---
+++ challenges/desafio22.js
@@ -0,0 +1,6 @@
+db.produtos.find(
+  { vendidos:
+    { $mod: [5, 0] },
+  },
+  { _id: false, nome: true, vendidos: true },
+);
