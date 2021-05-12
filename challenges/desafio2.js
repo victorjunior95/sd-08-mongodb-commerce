3c1bda8258b889c11b6f007259da4759d5354cef
Blucilha <blucilha.nascimento@gmail.com>
2021-05-12 15:37:25 -0300

    desafio 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,6 +2,6 @@ db.produtos.updateMany(
   {
     valorUnitario: { $exists: 0 } },
     { $set: { valorUnitario: NumberDecimal("0.00") },
-  }
+  },
 );
 db.produtos.find({ }, { valorUnitario: 1, nome: 1, _id: 0 });
