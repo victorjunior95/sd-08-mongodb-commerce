f1fa533684fa20998fb470b0e12e7e7f051e091d
Blucilha <blucilha.nascimento@gmail.com>
2021-05-12 19:16:32 -0300

    desafio 13 correçao

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -6,8 +6,8 @@ db.produtos.updateMany(
     ] },
   {
     $push: {
-      tags: "muito sódio" 
+      tags: "muito sódio",
     },
   },
 );
-db.produtos.find({ }, { _id:0, nome: 1, tags: 1 });
+db.produtos.find({ }, { _id: 0, nome: 1, tags: 1 });
