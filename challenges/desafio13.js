27597819c8871a88967260cb5205912587ed6f98
Blucilha <blucilha.nascimento@gmail.com>
2021-05-12 19:14:47 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {
+    $and: [
+      { "valoresNutricionais.tipo": "sódio" },
+      { "valoresNutricionais.percentual": { $gte: 40 } },
+    ] },
+  {
+    $push: {
+      tags: "muito sódio" 
+    },
+  },
+);
+db.produtos.find({ }, { _id:0, nome: 1, tags: 1 });
