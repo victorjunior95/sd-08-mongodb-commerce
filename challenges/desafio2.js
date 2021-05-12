9bd9fab2654362dee2560c003eca857bf7916f04
Giovanna Betti <giovannabetti@gmail.com>
2021-05-12 16:22:13 -0300

    fixing challenge 02

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { $set: { valorUnitario: "0.00", type: NumberDecimal } });
+db.produtos.updateMany({}, { $set: { valorUnitario: NumberDecimal("0.00") } });
 db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
