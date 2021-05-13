abab0c117f3aee9774263c52263b4f0a8d579784
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-05-13 15:37:41 -0300

    requisito 20 - remove campo curtidas do big mac

--- scripts/generate_result.sh
+++ scripts/generate_result.sh
@@ -30,7 +30,6 @@ print_results() {
 Print tests evaluation
 for entry in "$TRYBE_DIR/expected-results"/*
 do
-  entry="$TRYBE_DIR/expected-results/desafio$i"
   scripts/resetdb.sh "$DB_RESTORE_DIR"
   # Get challenge name
   chName=$(echo "$(basename $entry)" | sed -e "s/.js//g")
