dcfb4633f82d7829201a0720176012112ad429e2
victorjunior95 <victorjr.muchacho@gmail.com>
2021-05-13 12:05:29 -0300

    Challenge 6 and 7 completed

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -14,4 +14,4 @@ db.produtos.updateMany({ tags: { $regex: /bovino/i } }, { $inc: { avaliacao: 5 }
 
 db.produtos.updateMany({ tags: { $regex: /ave/i } }, { $inc: { avaliacao: 3 } });
 
-db.produtos.filter({}, { _id: 0, nome: 1, avaliacao: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
