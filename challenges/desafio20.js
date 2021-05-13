69855050511ce421f14f655493ae688d9a54834c
Blucilha <blucilha.nascimento@gmail.com>
2021-05-13 11:28:34 -0300

    desafios 20 correçao

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,2 +1,9 @@
-db.produtos.updateOne({ nome: "Big Mac", }, { $unset: { curtidas: "" }, },);
-db.produtos.find({ }, { _id: 0, nome: 1, curtidas: 1, },);
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $unset:
+    {
+      curtidas: "",
+    },
+  },
+);
+db.produtos.find({ }, { _id: 0, nome: 1, curtidas: 1 });
