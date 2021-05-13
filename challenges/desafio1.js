1e6619d07fdb796fcf946892c3f0d7bee5f4ca75
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-13 20:15:53 -0300

    fix: challenge 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,13 +1,13 @@
-db.produtos.updateMany{
+db.produtos.updateMany(
     {},
     {
         $set: {
-            criadoPor: "Ronald McDonald"
-        }
-    }
-};
+            criadoPor: "Ronald McDonald",
+        },
+    },
+);
 
 db.produtos.find(
     {},
-    { _id: 0, nome: 1, criadoPor: 1}
+    { _id: 0, nome: 1, criadoPor: 1 },
 );
