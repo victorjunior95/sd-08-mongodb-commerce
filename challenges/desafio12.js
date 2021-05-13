45388d4a67b29813f462a484758be1d48fc1a393
José Bernardo Johann <josebernardojohann@gmail.com>
2021-05-13 14:29:00 -0300

    Fixes challenge 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,11 +1,5 @@
 db.produtos.updateMany(
-  {},
-  {
-    $push: {
-      valoresNutricionais: {
-        $each: [],
-        $sort: { percentual: -1 },
-      },
-    },
-  },
+  { "valoresNutricionais.2.percentual": { $gte: 40 } },
+  { $push: { tags: "muito sódio" } },
 );
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 }); 
