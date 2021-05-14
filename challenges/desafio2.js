989e3c4e0df27df7285429ec2797020f931c7e16
Afonsofn <afonsoafn@gmail.com>
2021-05-14 09:00:31 -0300

    do requisito 2 ate o 5

---
+++ challenges/desafio2.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany(
+  {
+    valorUnitario: { $exists: 0 } },
+    { $set: { valorUnitario: NumberDecimal("0.00") },
+  },
+);
+db.produtos.find({ }, { valorUnitario: 1, nome: 1, _id: 0 });
