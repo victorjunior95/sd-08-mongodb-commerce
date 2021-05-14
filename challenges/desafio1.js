7cd7a00ce7581c22b7d85860048e4881bd9257b1
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-05-14 00:05:06 -0300

    req 1 without spacing

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,2 @@
-db.produtos.updateMany({}, { $set: {
-  criadoPor: "Ronald McDonald",
-} });
-
-db.produtos.find({}, {
-  nome: 1,
-  criadorPor: 1,
-  _id: 0,
-});
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+db.produtos.find({}, { nome: 1, criadorPor: 1, _id: 0 });
