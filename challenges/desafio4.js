17769d9ad7d0f3d28340302d282549115c87c257
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-13 22:35:40 -0300

    Requisito 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,9 @@
+
+db.produtos.updateOne(
+  { nome: { $regex: /big\smac/i } },
+  { $currentDate: { ultimaModificacao: true } },
+);
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
