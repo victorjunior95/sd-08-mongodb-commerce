a46cd93cb839ff858ec54407ee581432f18d15e7
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-13 22:32:00 -0300

    start: challenges 13 and 14

---
+++ challenges/desafio13.js
@@ -0,0 +1,27 @@
+db.produtos.updateMany(
+    {
+        valoresNutricionais: {
+            $elemMatch: {
+                tipo: "sódio",
+                percentual: { $gte: 40 },
+            },
+        },
+    },
+    {
+        $push: {
+            tags: "muito sódio",
+        },
+    },
+    {
+        upsert: true,
+    },
+);
+
+db.produtos.find(
+    {},
+    {
+        _id: 0,
+        nome: 1,
+        tags: 1,
+    },
+);
