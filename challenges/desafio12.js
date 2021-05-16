e5f79bcfa80c108c840455678de7f26ce5e1643d
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 20:38:29 -0300

    Desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -6,3 +6,9 @@ Crie uma query que faça em todos os documentos a ordenação dos elementos do a
 
 Crie uma query que retorne o nome e valoresNutricionais de todos os documentos.
 */
+db.produtos.updateMany(
+  {}, 
+  { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+);
+
+db.produtos.find({ }, { nome: true, valoresNutricionais: true, _id: false });
