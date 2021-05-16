9298ce8bf998b33c39e2ec0efb01fa3f519931ea
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 20:22:43 -0300

    Desafio 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -7,3 +7,9 @@ Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quart
 Crie uma query que retorne o nome e ingredientes de todos os documentos.
 
 */
+db.produtos.updateMany(
+  { nome: { $eq: "Quarteirão com Queijo" } }, 
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find({ }, { nome: true, ingredientes: true, _id: false });
