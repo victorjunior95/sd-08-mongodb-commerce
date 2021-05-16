02cd0809df8f59406374d2b7218b6e6ed380e7c7
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 20:33:48 -0300

    Desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -6,3 +6,9 @@ Crie uma query que faça tanto a inserção dos elementos combo e tasty no array
 
 Crie uma query que retorne o nome e tags de todos os documentos.
 */
+db.produtos.updateMany(
+  {}, 
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } },
+);
+
+db.produtos.find({ }, { nome: true, tags: true, _id: false });
