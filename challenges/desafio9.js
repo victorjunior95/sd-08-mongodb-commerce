f5401652226e2c4dab43aa69bd1c175b3520aabc
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 20:23:24 -0300

    Desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -6,3 +6,9 @@ Crie uma query que faça a remoção do último ingrediente no sanduíche Chedda
 
 Crie uma query que retorne o nome e ingredientes de todos os documentos.
 */
+db.produtos.updateMany(
+  { nome: { $eq: "Cheddar McMelt" } }, 
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find({ }, { nome: true, ingredientes: true, _id: false });
