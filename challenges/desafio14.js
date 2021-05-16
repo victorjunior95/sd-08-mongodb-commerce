08b1fa302fb78baf5125d49bd9d56c75bb04b683
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 20:55:32 -0300

    Desafio 14 e 15

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -6,3 +6,9 @@ Crie uma query que faça a adição do elemento contém sódio ao final do array
 
 Crie uma query que retorne o nome e tags de todos os documentos.
 */
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } }, 
+  { $push: { tags: "contém sódio" } },
+);
+
+db.produtos.find({ }, { nome: true, tags: true, _id: false });
