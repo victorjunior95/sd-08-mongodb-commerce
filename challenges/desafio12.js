f9a36de82a841d3bf45f8400a13a95abf264420f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-17 15:05:38 -0300

    desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
     {},
-    { $push: { valoresNutricionais: { $sort: { percentual: -1 } } } })
+    { $push: { valoresNutricionais: { $sort: { percentual: -1 } } } },)
 
 db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
