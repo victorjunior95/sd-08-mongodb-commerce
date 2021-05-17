0ba03914b77b88387e58dc445794e6386da871a8
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-17 14:12:09 -0300

    correçao de uso do sort. desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,7 +1,6 @@
 db.produtos.updateMany(
     {},
-    { $push: { tags: { $each: ["combo", "tasty"] } } },
-    { $sort: { tags: 1 } },
+    { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } },
 );
 //  { $push: { <field1>: { <modifier1>: <value1>, ... }, ... } }
 //  modifier $each -> adiciona varios valores no campo da matriz
@@ -10,4 +9,17 @@ db.produtos.updateMany(
 // { $sort: { <field1>: <sort order>, <field2>: <sort order> ... } }
 // order: 1 -> ascendente , 2 -> descendente
 // https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+
+// sort dentro do updateMany:
+/* db.collection.updateMany(
+    <filter>,
+    <update>,
+    {
+      upsert: <boolean>,
+      writeConcern: <document>,
+      collation: <document>,
+      arrayFilters: [ <filterdocument1>, ... ], -> é aqui que fica o sort
+      hint:  <document|string>        // Available starting in MongoDB 4.2.1
+    }
+ ) */
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
