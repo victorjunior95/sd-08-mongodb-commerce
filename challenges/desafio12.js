5669cc50863bf96b70d84d347101a46e4fed40ee
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-17 15:13:17 -0300

    requisito 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,5 +1,7 @@
 db.produtos.updateMany(
     {},
-    { $push: { valoresNutricionais: { $sort: { percentual: -1 } } } },)
-
+    { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+  );
+ // consultei o repositório de Arnaelcio para correção da sintaxe e uso do sort
+ // https://github.com/tryber/sd-08-mongodb-commerce/pull/71/files
 db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
