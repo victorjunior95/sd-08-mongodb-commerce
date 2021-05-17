e89017b9d14f0799d96b710a47c3b03e1ecb153c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-16 21:23:43 -0300

    desafio 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -9,4 +9,4 @@ db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
 
 db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
 
-db.produtos.find({}, { _id: 0, nome: 1, avialação: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
