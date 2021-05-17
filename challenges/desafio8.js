c6bd77d638fe68ccdcd3905de8ee7e82279fadc3
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-17 02:42:32 -0300

    desafio 8 - correçao comentario

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -2,7 +2,8 @@ db.produtos.updateMany(
     { nome: "Quarteirão com Queijo" },
     { $pop: { ingredientes: -1 } },
 );
-// $pop remove o primeiro elemento de um array
+// $pop remove o primeiro ou o ultimo elemento de um array
+// { $pop: { <field>: <-1 | 1>, ... } }
 // https://docs.mongodb.com/manual/reference/operator/update/pop/#mongodb-update-up.-pop
 
 db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
