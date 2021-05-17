dcc44159c7a0c02ab967216ef1916244c6187347
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-17 01:48:51 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+    { nome: { $ne: "McChicken" } },
+    { $addToSet: { ingredientes: "ketchup" } },
+  );
+//  consultei o repositório de Arnaelcio para a parte do condicional(addTSet).
+//  https://github.com/tryber/sd-08-mongodb-commerce/pull/71/files 
+// { $addToSet: { <field1>: <value1>, ... } } adiciona num um campo um valor, 
+// caso ele nao faça parte do campo.
+// https://docs.mongodb.com/manual/reference/operator/update/addToSet/
+  
+  
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
