8be90fc0b2fab498cb07386103573ddc506410b6
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 21:09:23 -0300

    Desafio 21

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,3 +1,4 @@
 /*
 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
 */
+db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } });
