5b3e268e442b3841f6553c2385a4d685420a68d5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-16 21:24:18 -0300

    Desafio 21: Retorna apenas o nome

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,4 +1,4 @@
 /*
 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
 */
-db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } });
+db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } }, { nome: true, _id: false });
