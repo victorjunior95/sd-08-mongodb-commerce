752127720a9939b0671c7ffd3225dd1d4806e017
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-18 18:23:27 -0300

    desafio17

---
+++ challenges/desafio17.js
@@ -0,0 +1,18 @@
+/* uso do text: {
+  $text:
+  {
+    $search: <string>,
+    $language: <string>,
+    $caseSensitive: <boolean>,
+    $diacriticSensitive: <boolean>
+  }
+} */
+// utilizaremos o search e o caseSensitive, embora este não seja necessário nesse caso.
+// https://docs.mongodb.com/manual/reference/operator/query/text/
+
+// é necessário criar um indice para suporta  a query de busca de texto
+// https://docs.mongodb.com/manual/core/index-text/
+
+db.produtos.createIndex({ text: "text" });
+
+db.produtos.find({ $text: { $search: "frango hamburguer", $caseSensitive: false }, }).count();
