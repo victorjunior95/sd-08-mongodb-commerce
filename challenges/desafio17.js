f444e7b781276a1f4cd24720ff083165fefaecc2
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-18 18:45:12 -0300

    desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -13,6 +13,15 @@
 // é necessário criar um indice para suporta  a query de busca de texto
 // https://docs.mongodb.com/manual/core/index-text/
 
-db.produtos.createIndex({ text: "text" });
+db.produtos.createIndex(
+    { descricao: "text" },
+    { default_language: "portuguese" },
+);
+//  consultei repositório de Arnaelcio para correção da criação do Índice.
+//  https://github.com/tryber/sd-08-mongodb-commerce/pull/71/files
+//  E para a correção em meu mongo, precisei utilizar os método dropIndex:
+//  https://docs.mongodb.com/manual/reference/method/db.collection.dropIndex/ ,
+//  E getIndexes() , para verificar o nome do Index á ser dropado
+//  https://docs.mongodb.com/manual/reference/method/db.collection.getIndexes/#mongodb-method-db.collection.getIndexes
 
 db.produtos.find({ $text: { $search: "frango hamburguer", $caseSensitive: false } }).count();
