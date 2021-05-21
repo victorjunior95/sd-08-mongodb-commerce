5636e16c9884486de4c851def9afe822149d7fdc
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-21 12:13:57 -0300

    desafio 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,3 @@
+db.produtos.updateOne({nome: "Big Mac"}, {$unset: { curtidas: 1}})
+// https://qastack.com.br/programming/6851933/how-to-remove-a-field-completely-from-a-mongodb-document
+// https://docs.mongodb.com/manual/reference/method/db.collection.updateOne/#mongodb-method-db.collection.updateOne
