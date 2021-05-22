74eb371a965b776a46306a0181b6894c5107dcd0
Daniel <marcio.daniel@msn.com>
2021-05-22 13:10:51 -0300

    6 - Done - Com Comentarios

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -7,6 +7,8 @@ db.produtos.updateMany(
 
   // o operador in é usado quando você quer passar mais de um valor para dar match,
   // algo parecido com includes do JS, eu verifico se o valor da key nome existe dentro do array, se sim dá bom.
-  // o método push por padrão coloca no final da lista, há um outro método que você pode dizer em qual posição por.
+  // o método push por padrão coloca no final da lista, há um outro método que você pode dizer em qual posição por. Atenção, pq esse método pode replicar informações dentro de um array, eu posso colocar vários bacons aqui dentro.
+
+  
 
 
