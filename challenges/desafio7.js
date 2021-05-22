89a1402c3e08aed4001dff1f5f43a2a80c735152
Daniel <marcio.daniel@msn.com>
2021-05-22 14:04:56 -0300

    7 - Fix Lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,9 +1,6 @@
-
 db.produtos.updateMany({}, {
-    $pull: { ingredientes: 'cebola'}
-    
+    $pull: { ingredientes: "cebola" },
   });
-  
   db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
 
 // O operador pull retira um valor do obj, depois verifica se não dá para puxar mais de um usando o in.
