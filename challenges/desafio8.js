f4da17b4502a8e85a95d4e4838630aea054d51a4
Daniel <marcio.daniel@msn.com>
2021-05-22 14:07:56 -0300

    8 - Fix Lint

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,10 +1,5 @@
 db.produtos.updateOne(
   { nome: "Quarteirão com Queijo" },
-  {
-    $pop: { ingredientes: -1 },
-  }
+  { $pop: { ingredientes: -1 } },
 );
-
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
-//Se liga na sintaxe do pop. Ele pode retirar o primeiro ou o último elemento de um array.
-//Então, vc passa depois do array você quer retirar o -1 se é para o começo e 1 se para o fina.
