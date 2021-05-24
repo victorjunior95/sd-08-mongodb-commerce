603485ffd823ab522f7fbc5ebd03f4e73fd6a1eb
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-24 14:48:44 -0300

    segunda parte ( query de busca ) dos deeafios 19 e 20

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -2,3 +2,4 @@ db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 // The $rename operator updates the name of a field and has the following form:
 // {$rename: { <field1>: <newName1>, <field2>: <newName2>, ... } }
 // https://docs.mongodb.com/manual/reference/operator/update/rename/
+db.produtos.find({}, { _id: 0, nome: 1, descicao: 1, descicaoSite: 1 });
