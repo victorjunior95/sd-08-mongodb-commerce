84936c34e7b7dab90365466c82d27a81a1dcb064
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-24 15:13:19 -0300

    desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,6 @@
+//  mod: seleciona documentos onde um valor de um campo
+//  dividido pro um divisor tenha um resto específico.
+//  { field: { $mod: [ divisor, remainder ] } }
+//  https://docs.mongodb.com/manual/reference/operator/query/mod/
+db.produtos.find({ vendidos: { $mod: [5, 0] }, },
+    { _id: 0, nome: 1, vendidos: 1 });
