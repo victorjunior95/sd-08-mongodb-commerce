cdcd64e508617fcf6344f0c2c9373a542c04152c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-24 15:16:06 -0300

    desafio 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,5 +2,5 @@
 //  dividido pro um divisor tenha um resto específico.
 //  { field: { $mod: [ divisor, remainder ] } }
 //  https://docs.mongodb.com/manual/reference/operator/query/mod/
-db.produtos.find({ vendidos: { $mod: [5, 0] }, },
+db.produtos.find({ vendidos: { $mod: [5, 0] } },
     { _id: 0, nome: 1, vendidos: 1 });
