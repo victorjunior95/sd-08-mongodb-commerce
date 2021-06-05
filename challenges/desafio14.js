9dd20558e25ca5fa13b1a641e84bdbc39dacb820
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 17:26:00 -0300

    desafio 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,27 @@
-14 - Adicione o elemento contém sódio ao final do array tags nos produtos em que o percentual de sódio
-seja maior do que 20 e menor do que 40.
-Duas queries, nesta ordem:
-  A. Crie uma query que faça a adição do elemento contém sódio ao final do array tags
-    nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
-  B. Crie uma query que retorne o nome e tags de todos os documentos.
-//   Query A: 
-//   Query B: 
+// 14 - Adicione o elemento contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
+
+//   Query A: Adiciona o elemento contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: {
+          $gt: 20,
+          $lt: 40,
+        },
+      },
+    },
+  },
+  { $push: { tags: "contém sódio" } },
+);
+
+//   Query B: Retorna o nome e tags de todos os documentos
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1
+  }
+);
