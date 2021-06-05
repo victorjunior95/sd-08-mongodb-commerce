5798d033ceb42036226d1cb122371e3ab022a693
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 16:09:48 -0300

    desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,8 +1,17 @@
-11 - Insira os elementos combo e tasty no array tags de todos os sanduíches e aproveite para deixar
-os elementos em ordem alfabética ascendente.
-Duas queries, nesta ordem:
-  A. Crie uma query que faça tanto a inserção dos elementos combo e tasty no array tags de todos
-    os sanduíches quanto a ordenação dos elementos de tags em ordem alfabética ascendente.
-  B. Crie uma query que retorne o nome e tags de todos os documentos.
-//   Query A: 
-//   Query B: 
+// 11 - Insira os elementos combo e tasty no array tags de todos os sanduíches e aproveite para deixar os elementos em ordem alfabética ascendente.
+
+//   Query A: Insere os elementos combo e tasty no array tags de todos os sanduíches e ordena os elementos de tags em ordem alfabética ascendente
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+);
+
+//   Query B: Retorna o nome e tags de todos os documentos
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
