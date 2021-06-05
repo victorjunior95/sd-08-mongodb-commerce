3f1150f7a553dc62cab1d113ba08eef6ae8ecf7e
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 15:51:49 -0300

    desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,3 +1,17 @@
 // 9 - Remova o último ingrediente do sanduíche Cheddar McMelt.
+
 //   Query A: Remove o último ingrediente no sanduíche Cheddar McMelt
+db.produtos.updateOne(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+
 //   Query B: Retorna o nome e ingredientes de todos os documentos
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1
+  }
+);
