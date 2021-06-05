39bdbb3132067404ac261f1465c2a56884c3ee3a
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 14:19:15 -0300

    desafio 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,3 +1,17 @@
 // 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+
 //   Query A: Adiciona o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+);
+
 //   Query B: Retorna o nome e valorUnitario de todos os produtos
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    valorUnitario: 1,
+  }
+);
