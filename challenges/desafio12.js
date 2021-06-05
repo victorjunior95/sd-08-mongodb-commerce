b80c511938fbcf03c3302bd129e41bd4095028c6
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 16:12:23 -0300

    desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,10 +1,18 @@
-12 - Ordene em todos os documentos os elementos do array valoresNutricionais pelo campo percentual
-de forma descendente.
-Duas queries, nesta ordem:
-  A. Crie uma query que faça em todos os documentos a ordenação dos elementos do array
-    valoresNutricionais pelo campo percentual de forma descendente.
-    Dica: mesmo sem adicionar nenhum novo elemento, para essa operação é necessário utilizar também
-      o modificador $each.
-  B. Crie uma query que retorne o nome e valoresNutricionais de todos os documentos.
-//   Query A: 
-//   Query B: 
+// 12 - Ordene em todos os documentos os elementos do array valoresNutricionais pelo campo percentual de forma descendente.
+
+//   Query A: Ordena em todos os documentos os elementos do array valoresNutricionais pelo campo percentual de forma descendente
+//     Dica: mesmo sem adicionar nenhum novo elemento, utilizar também o modificador $each
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      valoresNutricionais: {
+        $each: [],
+        $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+//   Query B: Retorna o nome e valoresNutricionais de todos os documentos
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
