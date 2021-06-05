96708c780feaac7671c57f7a792112989fdff3b9
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 15:46:03 -0300

    desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,3 +1,17 @@
 // 7 - Remova o item cebola de todos os sanduíches.
+
 //   Query A: Remove o item cebola em todos os sanduíches
+db.produtos.updateMany(
+  {},
+  { $pull: { ingredientes: "cebola" } },
+);
+
 //   Query B: Retorna o nome e ingredientes de todos os documentos
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1
+  }
+);
