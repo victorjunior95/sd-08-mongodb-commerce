87de524d2a20c94b82a28b7dc8ad1e10ce5809a6
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-05 15:48:56 -0300

    desafio 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,3 +1,17 @@
 // 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+
 //   Query A: Remove o primeiro ingrediente no sanduíche Quarteirão com Queijo
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
+
 //   Query B: Retorna o nome e ingredientes de todos os documentos
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1
+  }
+);
