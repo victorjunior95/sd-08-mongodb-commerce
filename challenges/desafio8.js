ed943affb64682511bc12d1f28d69bdb3f07e3be
marcela <marcela9908@gmail.com>
2021-06-06 01:12:59 -0300

    Correçao de bugs

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,12 +1,5 @@
 db.produtos.updateOne(
-<<<<<<< HEAD
   { nome: "Quarteirão com Queijo" }, 
   { $pop: { ingredientes: -1 } },
   );
   db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
-=======
-{ nome: "Quarteirão com Queijo" }, 
-{ $pop: { ingredientes: -1 } },
-);
-db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
->>>>>>> d27984e2eaa371b9cff3cbe7514a6f4f24c55bfc
