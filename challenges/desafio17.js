b72c2757ff75e8cb69e07e0a75bec182b0795b07
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-06 04:03:55 -0300

    desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,8 +1,10 @@
-17 - Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
-Duas queries, nesta ordem:
-  A. Crie uma query que faça a criação de um índice do tipo text no campo descricao
-    com o idioma padrão portuguese.
-  B. Crie uma query que retorne a quantidade de documentos que contêm as palavras
-    frango e hamburguer utilizando o operador $text.
-//   Query A: 
-//   Query B: 
+// 17 - Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
+
+//   Query A: Cria um índice do tipo text no campo descricao com o idioma padrão portuguese
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+//   Query B: Retorna a quantidade de documentos que contêm as palavras frango e hamburguer utilizando o operador $text
+db.produtos.count({ $text: { $search: "frango hamburguer" } });
