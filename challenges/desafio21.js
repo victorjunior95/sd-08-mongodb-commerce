a1988f0fbe394458c3ebe802792dc52e2d831d0f
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-06 04:04:44 -0300

    desafio 21

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,1 +1,15 @@
 // 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
+db.produtos.find(
+  {
+    $expr: {
+      $gt: [
+        "$curtidas",
+        "$vendidos",
+      ],
+    },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
