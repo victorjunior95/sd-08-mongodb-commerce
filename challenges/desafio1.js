a15650cafe7227c2ca9d39d4c332d28245991158
Rafael <rafaeljavaro750@gmail.com>
2021-06-06 22:14:51 -0300

    Desafio 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,6 @@ db.produtos.updateMany(
     {},
     { $set: { criadoPor: "Ronald MacDonald" } },
 );
-
 db.produtos.find(
     {},
     { nome: 1, criadoPor: 1, _id: 0 },
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,7 +2,6 @@ db.produtos.updateMany(
     {},
     { $addToSet: { valorUnitario: NumberDecimal("0.00") } },
 );
-
 db.produtos.find(
     {},
     { nome: 1, valorUnitario: 1, _id: 0 },
