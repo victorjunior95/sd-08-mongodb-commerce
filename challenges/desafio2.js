82351b4d1f757a3b9fe4af1f02136d0e4ff15742
IzeldaValeriano <zel.valeriano@gmail.com>
2021-06-17 15:49:39 -0300

    desafio a ao 7

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { valorUnitario: { $exists: 0 } },
-  { $set: { valorUnitario: NumberDecimal('0.00') } }
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
 
 db.produtos.find({}, { nome: 1, valorUnitario: 1 });
